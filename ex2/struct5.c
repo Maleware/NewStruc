aa62553ac40cdd67b3b2bbe24672b7997d494848
maxi <maxi.wittich@outlook.com>
2020-04-04 13:23:32 +0200

    Currently unfinished strcmp between abitrary many structures

---
+++ ex2/struct5.c
@@ -0,0 +1,67 @@
+#include "../input.h"
+#include "../output.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define MAX 30
+
+struct adres
+{
+	char fname[MAX];
+	char lname[MAX];
+	long zip;
+	char place[MAX];
+	int yob;
+};
+
+
+int cmp_structs(struct adres *str1, struct adres *str2)
+{
+	//First name equal
+	if(strcmp(str1->fname, str2->fname) == 0 && 
+	// Last name equal
+	strcmp(str1->lname, str2->lname) == 0 &&
+	// ZIP equal
+	(str1->zip-str2->zip) == 0 &&
+	// Place equal
+	strcmp(str1->place, str2->place) == 0 &&
+	// Year of Birth equal
+	(str1->yob-str2->yob) == 0 )
+		return 0; // Structures equal
+	else
+		return 1; // Strutures not equal
+}
+
+struct adres *input2(void)
+{
+	static struct adres *adresses;
+	adresses = malloc(sizeof(struct adres));
+	printf("First Name	:");
+	fgets(adresses->fname, MAX, stdin);
+	printf("Last Name	:");
+	fgets(adresses->lname, MAX, stdin);
+	printf("Zip Code	:");
+	do
+	{
+		scanf("%5ld",&adresses->zip);
+	}while(getchar()!='\n');
+	printf("Place		:");
+	fgets(adresses->place, MAX, stdin);
+	printf("Year of Birth	:");
+	do
+	{
+		scanf("%4d",&adresses->yob);
+	}while(getchar()!='\n');
+	return adresses;
+}
+
+void output5(struct adres *struct_ptr)
+{
+	printf("\n\nYour Informations are:\n");
+	printf("First Name......:%s",struct_ptr->fname);
+	printf("Last Name.......:%s",struct_ptr->lname);
+	printf("Zip Code........:%ld\n",struct_ptr->zip);
+	printf("Place...........:%s",struct_ptr->place);
+	printf("Year of Birth...:%d\n",struct_ptr->yob);
+}
