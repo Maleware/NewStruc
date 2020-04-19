22b393c6b15fb973fdde92d169ca5f3a4ef2dec8
maxi <maxi.wittich@outlook.com>
2020-04-19 17:31:46 +0200

    Added function delte and print to programm
    A bit buggie but i got it and i really want to go on ....

--- ex7/linear_list1.c
+++ ex7/linear_list1.c
@@ -90,6 +90,67 @@ void connect(char *n, char *f, int ad, int am, int ay, int hird,
 	}
 }
 
+void deletelist(char *wen)
+{
+	struct hired *pointer, *pointer1;
+
+	// is there an Element?
+	if(start != NULL)
+	{
+		// Is the first Element the one we are looking for?
+		if(strcmp(start->lname, wen) == 0)
+		{
+			pointer=start->next;
+			free(start);
+			start=pointer;
+		}
+		else
+		{
+			/* if it's not the first element in the list,
+			 * we have to search in the chain if the 
+			 * element is available */
+			pointer=start;
+			while(pointer->next != NULL)
+			{
+				pointer1=pointer->next;
+				// Is Adress of pointer1 the searched name?
+				if( strcmp(pointer1->lname, wen) == 0)
+				{
+					// if yes, then...
+					pointer->next=pointer1->next;
+					free(pointer1);
+					break;
+				}
+				pointer=pointer1;
+			} // End while
+		}	  // End else
+	} 		  // End if(start != NULL)
+	else
+		fprintf(stderr, "Data not available. Wrong name?");
+}
+
+void outputlist(void)
+{
+	struct hired *pointer = start;
+
+	printf("||==================================================="
+			"==============\n");
+	fprintf(stdout, "|%10cName%10c |Date of birth|Hired|Wage|\n",' ',' ');
+	printf("||==================================================="
+			"==============\n");
+
+	while(pointer != NULL)
+	{
+		fprintf(stdout, "|%12s,%-12s| %02d.%02d.%04d|"
+				"%02d.%02d.%04d|%06ld|\n",
+				pointer->lname,pointer->fname,pointer->age.day,
+				pointer->age.month, pointer->age.year,
+				pointer->hire.day, pointer->hire.month,
+				pointer->hire.year, pointer->wage);
+		pointer=pointer->next;
+	}
+}
+
 void inputlist(void)
 {
 	char lnam[MAX], fnam[MAX];
@@ -120,8 +181,33 @@ void inputlist(void)
 
 int linlist(void)
 {
-	while(getchar() != '#')
-		inputlist();
+	int select;
+	char dname[MAX];
+	while(1)
+	{
+		fprintf(stdout, "\n1 : Input\n");
+		fprintf(stdout, "2: Output\n");
+		fprintf(stdout, "3: Delete name\n");
+		fprintf(stdout, "0: End\n");
+		do
+		{
+			scanf("%d",&select);
+		}while(getchar() !='\n');
+		switch(select)
+		{
+			case 1: inputlist();
+				break;
+			case 2: outputlist();
+				break;
+			case 3: fprintf(stdout, "Input Name to delete: ");
+				fgets(dname, MAX, stdin);
+				deletelist(strtok(dname, "\n"));
+				break;
+			case 0: exit(EXIT_SUCCESS);
+			default: fprintf(stderr, "Wrong input");
+	
+		}
+	}
 	return EXIT_SUCCESS;
 }
 
