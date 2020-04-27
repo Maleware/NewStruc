3988bf5b749b12da0c134dd5a3270958a954a951
maxi <maxi.wittich@outlook.com>
2020-04-27 16:34:16 +0200

    Added as comment some usefull functions to delete a knot proper.

--- ex8/btree1.c
+++ ex8/btree1.c
@@ -14,7 +14,7 @@ int numb;
 
 KNOT *fitin(KNOT *pointer)
 {
-	if(pointer == NULL) // First entrie
+	if(pointer == NULL) // First entrie, or not end of tree
 	{
 		pointer=malloc(sizeof(KNOT));
 		if (pointer == NULL)
@@ -31,6 +31,68 @@ KNOT *fitin(KNOT *pointer)
 		pointer->right=fitin(pointer->right);
 	return (pointer);
 }
+
+/*
+void show_tree(KNOT *pointer)
+{
+	if(pointer != NULL)
+	{
+		fprintf(stdout, "\n%d ->", pointer->value);
+		show_tree(pointer->left);
+		show_tree(pointer->right);				
+	}
+}
+
+void delete_knot(KNOT **pointer)
+{
+	KNOT *temp;
+	int tempvalue;
+
+	if(global_root == *pointer)
+	{
+		fprintf(stderr, "Unable to delete root...\n");
+		return;
+	}
+	if( (*pointer) != NULL) // Leaf
+	{
+		if( (*pointer)->left == NULL && (*pointer)->right==NULL)
+		{
+			free(*pointer);
+			*pointer = NULL;
+		}
+		else if( (*pointer)->left==NULL) // Only right hand side can follow
+		{
+			temp = *pointer;
+			*pointer = (*pointer)->right;
+			free(temp);
+		}
+		else if( (*pointer)->right==NULL) // Only left hand side can follow
+		{
+			temp=*pointer;
+			*pointer = (*pointer)->left;
+			free(temp);
+		}
+		else // Looking for second follower replacement element
+		{
+			search_replace(&tempvalue, &( (*pointer)->right));
+			(*pointer)->value=tempvalue;
+		}
+	}
+}
+
+void searchknot(KNOT **pointer, int search)
+{
+	if( (*pointer) == NULL)
+		fprintf(stdout, "Empty tree...\n");
+	else if( (*pointer)->value == search) //Found knot
+		delete_knot(pointer);
+	else if( (*pointer)->value >= search)
+		searchknot(&((*pointer)->left), search);
+	else
+		searchknot(&((*pointer)->right), search);
+}
+
+*/ 
 
 int btree1(void)
 {
